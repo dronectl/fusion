9daa170cdddc19db8c86b200ffaa5d96471f2d43
ztnel <christian911@sympatico.ca>
2022-12-30 16:26:57 -0500

    dummy interface for testing

---
+++ fusion/interfaces/socket.py
@@ -0,0 +1,20 @@
+
+
+import socket
+from fusion.core.interface import Interface
+
+
+class TCPSocket(Interface):
+
+    def __init__(self, idn: str, target: str) -> None:
+        super().__init__(idn, target)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def get_host(self) -> str:
+        return '127.0.0.1'
+
+    def open(self) -> None:
+        self.socket.connect(self.target)
+
+    def close(self) -> None:
+        self.socket.close()
