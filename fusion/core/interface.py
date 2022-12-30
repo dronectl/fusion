167140ce0e0cf764f786d85d611e71ac4160b1a5
ztnel <christian911@sympatico.ca>
2022-12-30 16:09:46 -0500

    implement interface class as abc

--- fusion/core/interface.py
+++ fusion/core/interface.py
@@ -10,10 +10,21 @@ Copyright © 2022 dronectl. All rights reserved.
 import abc
 
 
-class Interface:
+class Interface(abc.ABC):
 
-    def __init__(self, idn: str) -> None:
+    def __init__(self, idn: str, target: str) -> None:
         self.idn = idn
+        self.target = target
+        self.host = self.get_host()
+        self.outgoing_transaction_log_fmt = f"{self.host} -> {self.target} | %s"
+        self.incoming_transaction_log_fmt = f"{self.host} <- {self.target} | %s"
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self) -> None:
+        self.close()
 
     @property
     def idn(self) -> str:
@@ -35,10 +46,54 @@ class Interface:
         """
         self.__idn = idn
 
-    @abc.abstractmethod
-    def connect(self) -> None:
-        ...
+    @property
+    def host(self) -> str:
+        """
+        Host address string
+
+        :return: host address identification
+        :rtype: str
+        """
+        return self.__host
+
+    @host.setter
+    def host(self, host: str) -> None:
+        """
+        Host address string
+
+        :param host: host address identification
+        :type host: str
+        """
+        self.__host = host
+
+    @property
+    def target(self) -> str:
+        """
+        Target device address string
+
+        :return: target device address identification
+        :rtype: str
+        """
+        return self.__target
+
+    @target.setter
+    def target(self, target: str) -> None:
+        """
+        Target device address string
+
+        :param target: target device address identification
+        :type target: str
+        """
+        self.__target = target
 
     @abc.abstractmethod
-    def disconnect(self) -> None:
-        ...
+    def open(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def close(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_host(self) -> str:
+        raise NotImplementedError
