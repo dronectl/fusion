9a94b063240e0071fc81ef7d149f2c17af1363d9
ztnel <christian911@sympatico.ca>
2022-12-30 16:14:13 -0500

    update toopology api

--- fusion/core/topology.py
+++ fusion/core/topology.py
@@ -3,12 +3,18 @@
 Fusion Core - Topology 
 ======================
 
+Topology describes the arrangmenet of devices on discoverable by the test platform. The topology 
+scopes the test execution by provisioning based on the availablility of devices.
+
 Copyright © 2022 dronectl. All rights reserved.
 """
 
-from typing import List
+import logging
 from pathlib import Path
+from typing import Dict, List
 
+from fusion.core.device import Device
+from fusion.core.registry import Registry
 from fusion.core.exceptions import TopologyError
 
 
@@ -17,7 +23,47 @@ class Topology:
     DEVICE_PATH = Path(__file__).parent.joinpath("devices").resolve()
     INTERFACE_PATH = Path(__file__).parent.joinpath("interfaces").resolve()
 
-    def __init__(self, devices: List[dict]) -> None:
+    def __init__(self) -> None:
+        self._logger = logging.getLogger(__name__)
+        self.devices = Registry({})
+
+    @property
+    def nodes(self) -> int:
+        """
+        Number of device nodes
+
+        :return: number of available devices
+        :rtype: int
+        """
+        return self.devices.size
+
+    @property
+    def devices(self) -> Registry[Device]:
+        """
+        Registered devices in topology
+
+        :return: device registry
+        :rtype: Registry[Device]
+        """
+        return self.__devices
+
+    @devices.setter
+    def devices(self, devices: Registry[Device]) -> None:
+        """
+        Registered devices in topology
+
+        :param devices: device registry 
+        :type devices: Registry[Device]
+        """
+        self.__devices = devices
+
+    def load(self, topology: Dict[str, List[Dict]]) -> None:
+        """
+        Load and instantiate topology instances
+        """
+        devices = topology.get('devices')
+        if devices is None:
+            raise TopologyError("Must specify at least 1 device in topology document.")
         for device in devices:
             qualname = device.get("type")
             if qualname is None:
